17e5972052ac60c97506fa0618d806f07e4dcbf6
Nihar Buliya <nihar.buliya@bajajfinserv.in>
2025-03-13 22:14:37 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Nihar Buliya on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
